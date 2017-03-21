7b2e7febe69451bd9bffe77a5cbb2814b17494c2
zoopy7 <cjdeleon7@yahoo.com>
2017-03-21 16:35:46 -0700

    Video 120
    Add Tick() to PlayerController
    -Add Tick method
    -Setup method for moving barrel towards crosshair

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,11 +3,6 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
-ATank* ATankPlayerController::GetControlledTank() const 
-{
-	return Cast<ATank>(GetPawn());
-}
-
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -20,4 +15,29 @@ void ATankPlayerController::BeginPlay()
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("Player Controller possessing: %s"), *ControlledTank->GetName());
 	}
+}
+
+// Called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank())
+	{
+		return;
+	}
+
+	// Get world location if linetrace through crosshair
+	// If it hits the landscape
+		//Tell controlled tank to aim at this point
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -14,8 +14,15 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 private:
 	ATank* GetControlledTank() const;
+
+	// Start the tank moving the barrel so that a shot would it where the crosshair intersects the world
+	void AimTowardsCrosshair();
 	
 	virtual void BeginPlay() override;
 };
