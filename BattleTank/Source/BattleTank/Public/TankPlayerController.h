7b9db06ec18cc9f5c3e4ef390314070626774f89
zoopy7 <cjdeleon7@yahoo.com>
2017-03-22 09:51:26 -0700

    Video 123
    Using DeprojectScreenToWorld
    -Converted crosshair screen position to world coordinates

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -29,6 +29,8 @@ private:
 	// Return an OUT parameter, true if hit landscape
 	virtual bool GetSightRayHitLocation(FVector& HitLocation) const;
 
+	bool GetLookDirection(FVector2D ScreenLocation, FVector & LookDirection) const;
+	
 	UPROPERTY(EditAnywhere)
 		float CrossHairXLocation = 0.5f;
 
