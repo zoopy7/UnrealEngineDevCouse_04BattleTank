c7567397754640dd473aadd126b4283e8e152955
zoopy7 <cjdeleon7@yahoo.com>
2017-04-08 22:31:14 -0700

    Video 135
    BlueprintSpawnableComponent()
    -Change TankBP to contain TankBarrel code
    -Hide Collision info in Blueprint

--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -8,7 +8,7 @@
 /**
  * 
  */
-UCLASS()
+UCLASS( meta = (BlueprintSpawnableComponent), hidecategories = ("Collision") )
 class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 {
 	GENERATED_BODY()
@@ -16,5 +16,14 @@ class BATTLETANK_API UTankBarrel : public UStaticMeshComponent
 public:
 	void Elevate(float DegreesPerSecond);
 	
+private:
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxDegreesPerSecond = 20.0f;
 	
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MaxElevationDegrees = 40.0f;
+	
+	UPROPERTY(EditAnywhere, Category = Setup)
+		float MinElevationDegrees = 0.0f;
+
 };
