3af6b24d1b3052f04dd357c3a5d4ce453b1d2fcf
zoopy7 <cjdeleon7@yahoo.com>
2017-05-03 20:18:20 -0700

    Video 151 - Apply ForceAtLocation() in Action
    -Add force to tracks

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -8,6 +8,12 @@ void UTankTrack::SetThrottle(float Throttle)
 	//auto Time = GetWorld()->GetTimeSeconds();
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+
+	// TODO clap actual throttle value so player can't over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied,ForceLocation);
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -18,5 +18,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
-	
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 400000.0f; // Assume 40 ton tank and 1g acceleration
 };
